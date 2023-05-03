fe43c4e8389cefead071acd822785f0aa170bd5b
121ananya <sinhaananya050@gmail.com>
2023-05-04 00:17:19 +0530

    child table changes

--- index.js
+++ index.js
@@ -183,6 +183,23 @@ app.get('/childData/:parentId', (req, res) => {
     });
   
     const tableHtml = `
+    <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      text-align: left;
+      padding: 8px;
+      border-bottom: 1px solid #ddd;
+    }
+    th {
+      background-color: #f2f2f2;
+    }
+    tr:hover {
+      background-color: #f5f5f5;
+    }
+  </style>
       <table>
         <thead>
           <tr>
